268d6377c4b9e748d0c0776eeec3e111d7b3e427
macktsawyer <macktsawyer@yahoo.com>
2021-05-24 12:26:45 -0400

    Scroll effect, footer made, info added

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let links = document.querySelectorAll(".menuList");
+
+links.forEach((item) =>{
+    item.addEventListener('click', ()=>
+    {
+        let ele = document.getElementById(item.getAttribute('data-link'));
+        ele.scrollIntoView({behavior:'smooth', block:'start'});
+    })
+})
+
+document.getElementById('ageYes').addEventListener('click', function() {
+    document.querySelector('#legalAgePrompt').style.display = 'none';
+    document.querySelector('#blackOutBlur').style.display = 'none';
+});
+
+document.getElementById('ageNo').addEventListener('click', function() {
+    document.querySelector('#legalAgePrompt').style.display = 'none';
+    window.location.replace('http://www.google.com');
+});
